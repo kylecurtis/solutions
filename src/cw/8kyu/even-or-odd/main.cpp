ac510a1ae4a037221e0765003a91b616d2644bcc
Kyle Curtis <30717363+kylecurtis@users.noreply.github.com>
2026-09-13 18:51:33 -0400

    Add even-or-odd

---
+++ src/cw/8kyu/even-or-odd/main.cpp
@@ -0,0 +1,16 @@
+#include "test_runner.hpp"
+
+#include <string>
+
+std::string even_or_odd(int number) {
+    return number % 2 == 0 ? "Even" : "Odd";
+}
+
+int main() {
+    TestRunner tests;
+    tests.check(even_or_odd(10) == "Even", "test 1");
+    tests.check(even_or_odd(-10) == "Even", "test 2");
+    tests.check(even_or_odd(33) == "Odd", "test 3");
+    tests.check(even_or_odd(0) == "Even", "test 4");
+    return tests.finish();
+}
